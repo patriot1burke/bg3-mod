9fa90c44a488a6941f1d9d8da72e61f99afaf2b9
Bill Burke <bburke@redhat.com>
2025-06-29 17:39:19 -0400

    cleanup

--- src/main/java/org/bg3/forge/model/Filter.java
+++
@@ -1,7 +0,0 @@
-package org.bg3.forge.model;
-
-import dev.langchain4j.model.output.structured.Description;
-
-public record Filter(@Description("A comparator for the value field") Operator operator, @Description("The value of the comparison") String value) {
-
-}
